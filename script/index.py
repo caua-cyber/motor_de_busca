81239ae49d9970a80724db0dabc47b2a1c5ec934
caua-cyber <=>
2026-09-23 19:43:45 -0300

    Adiciona botao

--- script/index.py
+++ script/index.py
@@ -6,8 +6,8 @@ def header():
     return titulo
 
 def selector():
-    entrada = lt.text_input("informe a ação ou ativo:")
-
+    entrada = lt.text_input("informe a ação ou ativo:" , placeholder="infome:")
+    button = lt.button("Procurar")
     return entrada
 
 header()
